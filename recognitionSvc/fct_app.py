7165e46c0ba0b39c2478839a142c1df2f3100b1b
robertsofianu <110179318+robertsofianu@users.noreply.github.com>
2024-04-15 21:17:19 +0300

    added docker file

---
+++ recognitionSvc/fct_app.py
@@ -0,0 +1,6 @@
+def fct_retun_zero():
+    return 0
+
+if __name__ == "__main__":
+    fct_retun_zero()
+    
--- recognitionSvc/tensorSvc.py
+++ recognitionSvc/tensorSvc.py
@@ -1,2 +1,15 @@
-print('Hello Wolrd 3')
-print('Hello Wolrd 5')
+from flask import Flask, jsonify
+from fct_app import *
+
+app = Flask(__name__)
+
+@app.route('/get_info')
+def route():
+    d = {
+        "object" : "masina"
+    }
+    return d
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
