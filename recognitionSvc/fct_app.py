39dc59da954e1711e8b34fbf69c1065f21999008
robertsofianu <110179318+robertsofianu@users.noreply.github.com>
2024-06-10 16:43:40 +0300

    add chenages

--- recognitionSvc/fct_app.py
+++
@@ -1,6 +0,0 @@
-def fct_retun_zero():
-    return 0
-
-if __name__ == "__main__":
-    fct_retun_zero()
-    
--- recognitionSvc/tensorSvc.py
+++ recognitionSvc/tensorSvc.py
@@ -1,15 +1,20 @@
 from flask import Flask, jsonify
-from fct_app import *
-
-app = Flask(__name__)
-
-@app.route('/get_info')
-def route():
-    d = {
-        "object" : "masina"
-    }
-    return d
+import tensorflow as tf
+# from tensorflow.keras import layers, models
+from tensorflow.python.keras import layers, models
 
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+# app = Flask(__name__)
+
+print(tf.__version__)
+
+# @app.route('/get_info')
+# def route():
+#     d = {
+#         "object" : "masina"
+#     }
+#     return d
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True, port=5000)
